c27a6f9ceeb38befa5f64055533e49b0f8cbaed7
bayendembaseye <bayendemba.seye@koonda.fr>
2019-03-18 20:43:24 +0000

    Boucle For

--- src/test/Testeur.java
+++ src/test/Testeur.java
@@ -16,7 +16,18 @@ public class Testeur {
 //        Affectation du ticket t au patient patient1.
         patient1.getTickets().add(t);
         patient1.getTickets().add(t1);
+        for (Ticket a : patient1.getTickets() )
+            System.out.println(a);
+        /*for (int i = 0; i < patient1.getTickets().size(); i++)
+            System.out.println(patient1.getTickets().get(i));*/
 
+//        Autre methode
+        /*patient1.getTickets().forEach(a -> {
+            System.out.println(a.toString());
+        });*/
+     /*   System.out.println("Affichage des tickets");
+        System.out.println(patient1.getTickets());
+        System.out.println("end Message");*/
        /* List<Ticket> maListe = new ArrayList<Ticket>();
         Ticket ticket = new Ticket(1L, new Date("03/01/2019"), "Infirmier", (double) 1000, patient);
         Ticket ticket1 = new Ticket(1L, new Date("12/12/2018"), "Dentiste", (double) 2000, patient1);
